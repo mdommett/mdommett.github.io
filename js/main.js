c41f45f25a01c23b5823682d226e488971b0009a
Michael Dommett <michaeldommett1@gmail.com>
2017-09-05 22:28:03 +0100

    changed theme to sustain

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-// Copyright 2014-2015 Twitter, Inc.
-// Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
-if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-    var msViewportStyle = document.createElement('style')
-    msViewportStyle.appendChild(
-        document.createTextNode(
-            '@-ms-viewport{width:auto!important}'
-        )
-    )
-    document.querySelector('head').appendChild(msViewportStyle)
-}
+/**
+ * Created by fabiomadeira on 25/02/15.
+ */
+// jQuery for page scrolling feature
+jQuery(document).ready(function(e) {
+    e(".scroll").click(function(t) {
+        t.preventDefault();
+        e("html,body").animate({
+            scrollTop: e(this.hash).offset().top
+        }, 1e3)
+    })
+});
+
+
